629340ace676a558b5a7792621fd914e91fbca34
liupeng <1269861316@qq.com>
2024-09-14 12:37:10 +0800

    fixed

--- parser.py
+++ parser.py
@@ -2,14 +2,56 @@
 Description: 
 Author: sky
 Date: 2024-09-14 09:56:53
-LastEditTime: 2024-09-14 10:41:49
+LastEditTime: 2024-09-14 12:15:48
 LastEditors: sky
 '''
 import requests
 import json
 import re
 from datetime import datetime
+import os
 
+
+import subprocess
+
+def executeCmd(cmd):
+    """
+        命令执行及回显传参
+        cmd:系统命令
+    """
+    (status, output) = subprocess.getstatusoutput(cmd)
+    return (status, output)
+
+def check_masscan():
+    '''
+    检查masscan
+    '''
+    cmd = 'masscan --version'
+    masscan_status = executeCmd(cmd)[0]
+
+    if masscan_status == 1:
+        return True
+    else:
+        if masscan_status != 0:
+            print('masscan未安装或未添加至环境变量')
+        return False
+
+def scan_ip():
+    """
+        扫描互联网上33445开放的IP
+    """
+    masscan_status = check_masscan()
+    if masscan_status:
+        cmd = f'sudo masscan -c myscan.conf'
+        # masscan是否执行完成
+        masscan_List = executeCmd(cmd)
+        masscan_active = masscan_List[0]
+        if masscan_active == 0:
+            return True
+        else:
+            print(masscan_List[1])
+            return False
+        
 def scan_website(address):
     '''
       Tailscale服务器扫描
@@ -40,19 +82,28 @@ def re_readme(ipStr):
         replacement = f'<!-- BLOG_START -->\n### 更新日期: {current_time}\n```\n{ipStr}\n```\n<!-- BLOG_END -->'
 
         new_content = re.sub(pattern, replacement, content, flags=re.DOTALL)
-        print(new_content)
         with open('README.md', 'w') as f:
             f.write(new_content)
         
+def gitAction():
+    executeCmd('git config --local user.email "action@github.com"')
+    executeCmd('git config --local user.name "GitHub Action"')
+    executeCmd('git add README.md')
+    executeCmd('git commit -m "Update README via GitHub Actions"')
+    executeCmd('git push')
+
 
 def main():
-    with open('scan.json', 'r') as f:
-        data = json.load(f)
-        ipStr = ''
-        for item in data:
-            ip = scan_website(item['ip'])
-            ipStr += f"{ip}\n"
-        re_readme(ipStr)
+    status = scan_ip()
+    if status:
+        with open('scan.json', 'r') as f:
+            data = json.load(f)
+            ipStr = ''
+            for item in data:
+                ip = scan_website(item['ip'])
+                ipStr += f"{ip}\n"
+            re_readme(ipStr)
+            gitAction()
 
 
 
